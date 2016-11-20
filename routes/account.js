4083e8f1e22cdd17b191a991f9a021867c11b58f
eabhijay1 <eabhjiay@gmail.com>
2016-11-19 22:00:38 -0500

    Added new views

--- routes/account.js
+++ routes/account.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Log-In' });
+  res.render('account', { title: 'Account' });
 });
 
 module.exports = router;

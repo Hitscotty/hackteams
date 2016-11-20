854ca5d68257eb24da431c238a89a27cbcb807ed
Jonathan <Jportorreal77@gmail.com>
2016-11-19 22:25:41 -0500

    scrapped mlh data

--- routes/index.js
+++ routes/index.js
@@ -4,32 +4,8 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var options = {
-    "method": "GET",
-    "hostname": "mlh.io",
-    "port": null,
-    "path": "/seasons/na-2017/events",
-    "headers": {
-      "cache-control": "no-cache",
-      "postman-token": "42e03d13-9d8e-6e72-650b-863cabbe4c2e"
-    }
-  };
 
-  var req = http.request(options, function (res) {
-    var chunks = [];
-
-    res.on("data", function (chunk) {
-      chunks.push(chunk);
-    });
-
-    res.on("end", function () {
-      var body = Buffer.concat(chunks);
-      console.log(body.toString());
-    });
-  });
-
-  req.end();
-  res.render('index', { title: 'AWS Demo', body: 'body' });
+  res.render('index', { title: 'AWS Demo', bdy: 'body' });
 });
 
 module.exports = router;
